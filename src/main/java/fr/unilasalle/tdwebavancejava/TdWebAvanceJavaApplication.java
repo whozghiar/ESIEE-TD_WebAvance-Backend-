f8a835c2420687fa0f8156aa9e9a02f04b732ae1
Irlando-MSI <73164809+whozghiar@users.noreply.github.com>
2024-03-21 11:01:01 +0100

    TD WebAvance Partie Java Initialisation

---
+++ src/main/java/fr/unilasalle/tdwebavancejava/TdWebAvanceJavaApplication.java
@@ -0,0 +1,13 @@
+package fr.unilasalle.tdwebavancejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdWebAvanceJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdWebAvanceJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/unilasalle/tdwebavancejava/TdWebAvanceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.unilasalle.tdwebavancejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdWebAvanceJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
